37fa648233eb6ffb03142ca17b9eea52e6671826
Adam Weeks <me@adamweeks.com>
2016-05-18 22:54:59 -0400

    Slider board init

--- src/app/slider-game/slider-game.component.ts
+++ src/app/slider-game/slider-game.component.ts
@@ -10,14 +10,45 @@ import {SliderGamePiece} from '../shared/slider-game-piece.class';
   directives: [SliderGamePieceComponent]
 })
 export class SliderGameComponent implements OnInit {
-  public gamePieces: SliderGamePiece[] = [];
-  @Input() numberOfPieces: number;
-  constructor() {}
+  gamePieces: SliderGamePiece[] = [];
+  gameRows: SliderGameBoardRow[] = [];
+
+  numberOfPieces: number;
+  numberOfRows: number;
+  numberOfColumns: number;
+  constructor() {
+    this.numberOfPieces = 9;
+    this.numberOfRows = 3;
+    this.numberOfColumns = 3;
+  }
 
   ngOnInit() {
+    // Create game board spots
+    for(let row = 0; row < this.numberOfRows; row++) {
+      let columns = [];
+      for(let col = 0; col < this.numberOfColumns; col++) {
+        let spot = new SliderGameBoardSpot(row, col);
+        columns.push(spot);
+      }
+      this.gameRows.push(new SliderGameBoardRow(columns, row));
+    }
+    // Create game pieces
     for(var myNumber = 1; myNumber < this.numberOfPieces; myNumber++) {
       this.gamePieces.push(new SliderGamePiece(myNumber.toString(), myNumber-1));
     }
   }
 
+}
+
+class SliderGameBoardRow {
+  constructor(public columns: SliderGameBoardSpot[], rowNumber: number) {
+
+  }
+}
+
+class SliderGameBoardSpot {
+  public gamePiece:SliderGamePiece;
+  constructor(public row:number, public column:number){
+    console.log('game spot: ' + row + ',' + column);
+  }
 }
